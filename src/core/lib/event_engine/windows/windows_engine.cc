stungkit/grpc

commit b48188fcbca50be15100cca2bb6309e798688135

[EventEngine] Fix a Windows race that causes an assertion error (#41563)
This should fix #40405 by fixing the race described in https://github.com/grpc/grpc/issues/40405#issuecomment-3849835015.

<!--

If you know who should review your pull request, please assign it to that
person, otherwise the pull request would get assigned randomly.

If your pull request is for a specific language, please add the appropriate
lang label.

-->

Closes #41563

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/41563 from murgatroid99:windows_engine_race_fix d8415d054818af8646e5a20d7182db8293d79409
PiperOrigin-RevId: 891768950

src/core/lib/event_engine/windows/windows_engine.cc
@@ -398,11 +398,15 @@ void WindowsEventEngine::OnConnectCompleted(
       erased_handles =
           known_connection_handles_.erase(state->connection_handle());
     }
-    if (erased_handles != 1 || !Cancel(state->timer_handle())) {
+    if (erased_handles != 1) {
       GRPC_TRACE_LOG(event_engine, INFO)
           << "Not accepting connection since the deadline timer has fired";
       return;
     }
+    // Cancel the deadline timer, to avoid having a dangling handle. It is OK
+    // if this fails, meaning that the deadline timer has already fired,
+    // because it won't cancel the connection attempt.
+    Cancel(state->timer_handle());
     // Release refs held by the deadline timer.
     state->AbortDeadlineTimer();
     const auto& overlapped_result = state->socket()->write_info()->result();

commit 76841c61eefea3cf3217fa6aed844fb04e15bf63

[EventEngine] Fix a potential use-after-free error on Windows (#42078)
This issue was introduced by #41563. That PR allowed `OnConnectCompleted` to proceed past a failed `Cancel(state->timer_handle())`, and the next line releases the timer callback. That is a problem because the timer callback can still run, though it won't actually do anything beyond see that the `OnConnectedCallback` ran and then release itself. The solution is to only release the timer callback if the timer was successfully cancelled, and otherwise count on the timer callback to clean itself up.

`OnConnectCallback` doesn't interact with the timer after that point, so I'm fairly certain that this is the last problem of its kind here.

<!--

If you know who should review your pull request, please assign it to that
person, otherwise the pull request would get assigned randomly.

If your pull request is for a specific language, please add the appropriate
lang label.

-->

Closes #42078

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/42078 from murgatroid99:windows_event_engine_race_fix_2 ec74a2e49f16737466bb1ea67b926a5eb7cd2de4
PiperOrigin-RevId: 910905066

src/core/lib/event_engine/windows/windows_engine.cc
@@ -406,9 +406,13 @@ void WindowsEventEngine::OnConnectCompleted(
     // Cancel the deadline timer, to avoid having a dangling handle. It is OK
     // if this fails, meaning that the deadline timer has already fired,
     // because it won't cancel the connection attempt.
-    Cancel(state->timer_handle());
-    // Release refs held by the deadline timer.
-    state->AbortDeadlineTimer();
+    if (Cancel(state->timer_handle())) {
+      // Release refs held by the deadline timer.
+      // Only call this if we successfully cancel the deadline timer.
+      // Otherwise, we needto let the deadline timer run, and it will
+      // release the refs itself.
+      state->AbortDeadlineTimer();
+    }
     const auto& overlapped_result = state->socket()->write_info()->result();
     if (!overlapped_result.error_status.ok()) {
       state->socket()->Shutdown(DEBUG_LOCATION, "ConnectEx failure");